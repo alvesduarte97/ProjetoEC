0c2b531aec8c9bbb6f740e8dd9ed11a38f12732e
PC <PC@PC-PC>
2018-02-27 21:54:14 -0300

    Bibliotecas fixadas na pasta e imagens na tela de gerar lista.

--- AvaliacoesEC/src/com/br/avaliacoes/ec/telas/GerarExcelTela.java
+++ AvaliacoesEC/src/com/br/avaliacoes/ec/telas/GerarExcelTela.java
@@ -7,26 +7,28 @@ import com.br.avaliacoes.ec.excecoes.BancoException;
 import com.br.avaliacoes.ec.fachada.FachadaImp;
 import com.br.avaliacoes.ec.modelo.Desafios;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
 import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
+import javax.swing.JList;
+import java.awt.Font;
+import javax.swing.JSeparator;
+import javax.swing.SwingConstants;
 
 public class GerarExcelTela extends BaseOrgTela {
-	private JTextField txtNomeDesafio;
 
 	
 	public GerarExcelTela() {
 		
-		txtNomeDesafio = new JTextField();
-		txtNomeDesafio.setBounds(43, 101, 86, 20);
-		add(txtNomeDesafio);
-		txtNomeDesafio.setColumns(10);
-		
-		JButton btnGerar = new JButton("gerar");
+		JButton btnGerar = new JButton("Gerar Lista");
+		btnGerar.setFont(new Font("Tahoma", Font.BOLD, 15));
 		btnGerar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				String nomeDesafio = txtNomeDesafio.getText();
@@ -48,8 +50,30 @@ public class GerarExcelTela extends BaseOrgTela {
 				
 			}
 		});
-		btnGerar.setBounds(156, 100, 89, 23);
+		
+		JList list = new JList();
+		list.setBounds(260, 186, 198, 203);
+		add(list);
+		
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setBounds(-68, 469, 877, 2);
+		add(separator_1);
+		
+		JLabel lblSelecioneUmDesafio = new JLabel("Selecione um desafio para gerar a lista.");
+		lblSelecioneUmDesafio.setHorizontalAlignment(SwingConstants.CENTER);
+		lblSelecioneUmDesafio.setBounds(260, 391, 198, 14);
+		add(lblSelecioneUmDesafio);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(-68, 169, 877, 2);
+		add(separator);
+		btnGerar.setBounds(260, 416, 198, 42);
 		add(btnGerar);
-
+		JLabel Imagem3 = new JLabel("");
+		ImageIcon icone3 = new ImageIcon(LoginTela.class.getResource("/img/Fundo2.jpg"));
+		Imagem3.setBounds(0, 17, 741, 668);
+		Image imagi3 = icone3.getImage().getScaledInstance(Imagem3.getWidth(), Imagem3.getHeight(), Image.SCALE_SMOOTH);
+		Imagem3.setIcon(new ImageIcon(imagi3));
+		add(Imagem3);
 	}
 }
